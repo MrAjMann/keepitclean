c433a182182f3f5063cb998c96bd263a90b2a5e3
MrAjMann <anthonyjmann87@gmail.com>
2023-09-17 05:38:15 +1000

    Landing page is now responsive

--- src/components/ui/GetInTouchCard.jsx
+++ src/components/ui/GetInTouchCard.jsx
@@ -1,13 +1,13 @@
 function GetInTouchCard() {
 	return (
-		<div className='relative w-full mb-16 md:mb-0 lg:absolute lg:-top-20 lg:right-8 lg:-translate-x-1/3 bg-[#B68467]  lg:h-4/6 lg:w-1/4 '>
+		<div className=' w-full  pb-16  md:pb-8 lg:absolute lg:-top-20 lg:right-8 lg:-translate-x-1/3 bg-[#B68467]   lg:w-1/4'>
 			<div className='relative w-full'>
-				<div className='flex flex-start md:justify-center w-full'>
+				<div className='flex flex-start justify-center w-full'>
 					<h4 className='text-white p-4 font-medium text-2xl'>Get in Touch</h4>
 				</div>
-				<form action='' className=' my-12 md:w-72  md:m-auto w-full'>
-					<ul className='flex flex-col space-y-8 mx-8 md:mx-0'>
-						<li className='flex flex-col gap-2'>
+				<form action='' className=' w-full mt-16 max-w-xl m-auto'>
+					<ul className=' flex flex-col  px-8 md:mx-0 gap-4  justify-center'>
+						<li className='flex flex-col gap-2 '>
 							<label htmlFor='name' className='text-white'>
 								Name
 							</label>
@@ -19,7 +19,7 @@ function GetInTouchCard() {
 								className='bg-transparent border-b-[1px] border-[#DBBBA7] placeholder-[#DBBBA7] outline-none text-white text-medium'
 							/>
 						</li>
-						<li className='flex flex-col space-y-2'>
+						<li className='flex flex-col gap-2'>
 							<label htmlFor='email' className='text-white'>
 								Email
 							</label>
@@ -31,7 +31,7 @@ function GetInTouchCard() {
 								className='bg-transparent border-b-[1px] border-[#DBBBA7] placeholder-[#DBBBA7] outline-none text-white text-medium'
 							/>
 						</li>
-						<li className='flex flex-col space-y-2'>
+						<li className='flex flex-col gap-2'>
 							<label htmlFor='phone' className='text-white'>
 								Phone
 							</label>
@@ -43,7 +43,7 @@ function GetInTouchCard() {
 								className='bg-transparent border-b-[1px] border-[#DBBBA7] placeholder-[#DBBBA7] outline-none text-white text-medium'
 							/>
 						</li>
-						<li className='flex flex-col space-y-2 pb-8'>
+						<li className='flex flex-col  '>
 							<label htmlFor='name' className='text-white '>
 								Message
 							</label>
@@ -51,7 +51,7 @@ function GetInTouchCard() {
 								type='text'
 								name='message'
 								id=''
-								className='bg-transparent border-b-[1px] border-[#DBBBA7]  outline-none text-white text-medium resize-none'
+								className='bg-transparent border-b-[1px] border-[#DBBBA7]  outline-none text-white text-medium resize-none h-8'
 							/>
 						</li>
 					</ul>
