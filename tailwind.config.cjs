f1d36096517cb190367b2120e4aa3e7a0b053e93
MrAjMann <anthonyjmann87@gmail.com>
2023-09-17 16:19:39 +1000

    Now responsive on xl widescreen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				"3xl": "1792px",
+				"4xl": "2048",
+			},
+		},
 	},
 	plugins: [],
-}
+};
